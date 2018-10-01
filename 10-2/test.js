38a5716d638a43628ee0899eab6db46e60f59fbd
duankai8757 <duankai8757@163.com>
2018-10-02 03:11:22 +0800

    闭包demo

--- 10-2/test.js
+++ 10-2/test.js
@@ -1,2 +1,11 @@
-let arr=[1,2,3,4,55];
-console.log(arr.includes(2));
+/*let arr=[1,2,3,4,55];
+console.log(arr.includes(2));*/
+/*function a(b) {
+    return function (c) {
+        return b+c;
+    }
+}*/
+// 箭头函数
+
+let a = b => c => b+c;//高阶函数（大于等于2箭头）
+console.log(a(3)(4))
